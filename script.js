f1b20c1456f2430700825d049db9a5b2d04f8ce0
kiza311 <zakiramliza03@gmail.com>
2025-07-21 01:14:48 +0700

    Add files via upload
    Upload file website BARETO pertama

---
+++ script.js
@@ -0,0 +1,12 @@
+// Scroll animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('.animate-fade').forEach((el) => {
+  observer.observe(el);
+});
